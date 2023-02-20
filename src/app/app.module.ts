d046a9506affffeee6aa01168600d9276c845adc
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-20 14:31:26 -0700

    Add missing imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -9,19 +8,32 @@ import { AnimationComponent } from './components/animation/animation.component';
 import { LottieModule } from 'ngx-lottie';
 import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { GoogleButtonComponent } from './components/google-button/google-button.component';
 
 // TODO This is a function necessary for lottie (is there a better place for this?)
 export function playerFactory(): any {
   return import('lottie-web');
 }
 @NgModule({
-  declarations: [AppComponent, AnimationComponent],
+  declarations: [
+    AppComponent,
+    AnimationComponent,
+    LoginComponent,
+    SignupComponent,
+    GoogleButtonComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
     LottieModule.forRoot({ player: playerFactory }),
+    FormsModule,
   ],
   providers: [AuthService],
   bootstrap: [AppComponent],
