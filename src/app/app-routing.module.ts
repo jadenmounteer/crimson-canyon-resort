e28bbeaa1a7ffb2f5aedf0b1f4e419a7e0a3fefe
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-20 14:42:13 -0700

    Updates to nav component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //   {
+  //   path: '',
+  //   component: HomePageComponent,
+  // },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  // {
+  //   path: 'exercise-library',
+  //   component: ExerciseLibraryComponent,
+  //   canActivate: [AuthGuard],
+  // },
+  // {
+  //   path: 'coming-soon',
+  //   component: FeatureComingSoonComponent,
+  //   canActivate: [AuthGuard],
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
