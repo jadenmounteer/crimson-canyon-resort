b24dddcf726db22e41ebc38e8d01ad0cd210e6b2
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-20 14:03:35 -0700

    Add dependencies

---
+++ src/scripts/deploy-to-github-pages.sh
@@ -0,0 +1,4 @@
+cd /Users/jadenmounteer/git/crimson-canyon-resort
+npm i angular-cli-ghpages --save-prod
+ng build --base-href "https://jadenmounteer.github.io/crimson-canyon-resort/"
+npx angular-cli-ghpages --dir=dist/crimson-canyon-resort
