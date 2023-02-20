53abae3a94c8e2f4bf3315567183b1dbb480eaf2
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-20 14:38:56 -0700

    Add nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/auth/login/login.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
 import { GoogleButtonComponent } from './components/google-button/google-button.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 // TODO This is a function necessary for lottie (is there a better place for this?)
 export function playerFactory(): any {
@@ -25,6 +27,7 @@ export function playerFactory(): any {
     LoginComponent,
     SignupComponent,
     GoogleButtonComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,6 +37,7 @@ export function playerFactory(): any {
     AngularFireModule.initializeApp(environment.firebase),
     LottieModule.forRoot({ player: playerFactory }),
     FormsModule,
+    FontAwesomeModule,
   ],
   providers: [AuthService],
   bootstrap: [AppComponent],
---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { IconService } from 'src/app/services/icon.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss'],
+})
+export class NavComponent implements OnInit, OnDestroy {
+  public isAuth: boolean = false;
+  private authSubscription!: Subscription;
+
+  protected isMenuCollapsed: boolean = true;
+
+  constructor(public icon: IconService, public authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => {
+        this.isAuth = authStatus;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
+}
