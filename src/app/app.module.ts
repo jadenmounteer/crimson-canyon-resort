a7ec26be1c9906778d49bfc95f041196c658dabe
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-20 14:24:25 -0700

    Fix import bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,23 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AuthService } from './components/auth/auth.service';
 import { AnimationComponent } from './components/animation/animation.component';
+import { LottieModule } from 'ngx-lottie';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
 
+// TODO This is a function necessary for lottie (is there a better place for this?)
+export function playerFactory(): any {
+  return import('lottie-web');
+}
 @NgModule({
   declarations: [AppComponent, AnimationComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    LottieModule.forRoot({ player: playerFactory }),
+  ],
   providers: [AuthService],
   bootstrap: [AppComponent],
 })
