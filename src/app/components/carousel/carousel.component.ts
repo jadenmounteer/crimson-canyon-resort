fd47d2050fe563a6a60154965d7bcb96744b1b02
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-22 08:06:42 -0700

    Remove unused imports

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-carousel',
