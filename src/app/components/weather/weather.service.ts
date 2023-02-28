d46407caf03db817647d7d2e0346866b2b6f3f7a
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-28 07:31:07 -0700

    Install moment

--- src/app/components/weather/weather.service.ts
+++ src/app/components/weather/weather.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
-
-  constructor() { }
+  constructor() {}
 }
