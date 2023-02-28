6ae4a713ba5f3aed2ecac8f4bb375db09efef59a
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-02-28 07:33:33 -0700

    Import the api key into weather service

--- src/app/components/weather/weather.service.ts
+++ src/app/components/weather/weather.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as moment from 'moment';
+import { environment } from 'src/environments/environment';
+const currentWeatherApiKEy: string = environment.currentWeatherApiKEy;
 
 @Injectable({
   providedIn: 'root',
