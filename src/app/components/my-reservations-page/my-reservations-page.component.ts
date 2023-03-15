22337aa49a97fd88c7bbcf92ac71b897aeecc03a
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-03-14 20:59:33 -0600

    Add html to my reservations page

--- src/app/components/my-reservations-page/my-reservations-page.component.ts
+++ src/app/components/my-reservations-page/my-reservations-page.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-my-reservations-page',
   templateUrl: './my-reservations-page.component.html',
-  styleUrls: ['./my-reservations-page.component.scss']
+  styleUrls: ['./my-reservations-page.component.scss'],
 })
 export class MyReservationsPageComponent implements OnInit {
+  protected contentLoaded: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.contentLoaded = true;
+    }, 1000);
   }
-
 }
