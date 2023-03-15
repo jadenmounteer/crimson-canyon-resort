3ca25ab3a25bd57016481fd80f0374241b93cba2
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-03-14 21:36:19 -0600

    Add home page component back to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { ReserveTripPageComponent } from './components/reserve-trip-page/reserve
 const routes: Routes = [
   {
     path: '',
-    component: ReservationBookedPageComponent,
+    component: HomePageComponent,
   },
   {
     path: 'login',
