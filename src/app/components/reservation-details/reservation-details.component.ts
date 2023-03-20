fda9d890a391994010675721e484cd258175b761
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-03-20 08:16:53 -0600

    Add auth to reservation details

--- src/app/components/reservation-details/reservation-details.component.ts
+++ src/app/components/reservation-details/reservation-details.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { IconService } from 'src/app/services/icon.service';
 import { ReservationsService } from 'src/app/services/reservations.service';
 import { Reservation } from 'src/app/types/reservation';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-reservation-details',
@@ -14,14 +16,28 @@ import { Reservation } from 'src/app/types/reservation';
 export class ReservationDetailsComponent implements OnInit {
   public contentLoaded: boolean = false;
   protected reservation!: Reservation;
+  public isAuth: boolean = false;
+  private authSubscription!: Subscription;
 
   constructor(
     public icon: IconService,
     private route: ActivatedRoute,
-    private reservationsService: ReservationsService
+    private reservationsService: ReservationsService,
+    private authService: AuthService
   ) {}
 
   ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => {
+        this.isAuth = authStatus;
+      }
+    );
+    if (this.authService.isAuthenticated) {
+      this.isAuth = true;
+    } else {
+      this.isAuth = false;
+    }
+
     this.route.paramMap.subscribe((params) => {
       let id: string | null = params.get('id');
       this.reservation = this.reservationsService.getReservation(id);
