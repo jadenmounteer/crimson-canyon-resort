d1fbe1c3adf93beee2a772d91206944b605811dc
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-04-10 07:36:35 -0600

    Add initial administration page component

--- src/app/administration/administration.module.ts
+++ src/app/administration/administration.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdministrationRoutingModule } from './administration-routing.module';
+import { AdministrationPageComponent } from './administration-page/administration-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdministrationPageComponent
+  ],
   imports: [
     CommonModule,
     AdministrationRoutingModule
