57e1285fc9554fbb00f9cfd72d2b16a98b3117cc
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-04-20 08:00:37 -0600

    implement on delete

--- src/app/administration/reservations-by-user/reservations-by-user.component.ts
+++ src/app/administration/reservations-by-user/reservations-by-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ConfirmModalComponent } from 'src/app/components/confirm-modal/confirm-modal.component';
 import { ReservationsService } from 'src/app/services/reservations.service';
 import { Reservation } from 'src/app/types/reservation';
 
@@ -10,7 +12,10 @@ import { Reservation } from 'src/app/types/reservation';
 export class ReservationsByUserComponent implements OnInit {
   protected contentLoaded: boolean = false;
   protected reservations: Array<Reservation> = [];
-  constructor(protected reservationsService: ReservationsService) {}
+  constructor(
+    protected reservationsService: ReservationsService,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     this.reservationsService.allReservationsChanged.subscribe(
@@ -21,5 +26,15 @@ export class ReservationsByUserComponent implements OnInit {
     );
 
     this.reservationsService.fetchAllReservations();
+  }
+
+  public onDeleteReservation(reservation: Reservation) {
+    const modalRef = this.modalService.open(ConfirmModalComponent);
+    modalRef.componentInstance.message = `Are you sure you want to cancel the ${reservation.familyName} family reservation?`;
+    modalRef.result.then((result) => {
+      if (result === 'Yes') {
+        this.reservationsService.deleteReservation(reservation);
+      }
+    });
   }
 }
