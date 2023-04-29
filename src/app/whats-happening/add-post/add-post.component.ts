bf0df8419c332097e6704664fb6230a03f78e549
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-04-29 09:02:49 -0600

    Finish upload file logic for now

--- src/app/whats-happening/add-post/add-post.component.ts
+++ src/app/whats-happening/add-post/add-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { Observable, catchError, concatMap, last, tap, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-add-post',
@@ -7,6 +8,9 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
   styleUrls: ['./add-post.component.scss'],
 })
 export class AddPostComponent implements OnInit {
+  protected percentageChanges$!: Observable<number | undefined>;
+  protected iconURL: string = '';
+
   constructor(private storage: AngularFireStorage) {}
 
   ngOnInit(): void {}
@@ -16,10 +20,25 @@ export class AddPostComponent implements OnInit {
     // TODO Put the post ID into this filepath to have a separate folder per post.
     // const filePath = `posts/${this.postID}`;
     const filePath = `posts/${file.name}`;
+
+    // TODO We may want to only upload this when we submit the form
     const task = this.storage.upload(filePath, file, {
       cacheControl: 'max-age=2592000,public',
     });
 
-    task.snapshotChanges().subscribe();
+    this.percentageChanges$ = task.percentageChanges();
+
+    task
+      .snapshotChanges()
+      .pipe(
+        last(),
+        concatMap(() => this.storage.ref(filePath).getDownloadURL()),
+        tap((url) => (this.iconURL = url)),
+        catchError((err) => {
+          console.log(err);
+          return throwError(err);
+        })
+      )
+      .subscribe();
   }
 }
