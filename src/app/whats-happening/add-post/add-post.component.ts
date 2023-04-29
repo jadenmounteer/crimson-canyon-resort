19563414ca940831408e09517010689d5a7d21f9
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-04-29 08:39:20 -0600

    Move upload logic to add post

--- src/app/whats-happening/add-post/add-post.component.ts
+++ src/app/whats-happening/add-post/add-post.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
-  styleUrls: ['./add-post.component.scss']
+  styleUrls: ['./add-post.component.scss'],
 })
 export class AddPostComponent implements OnInit {
+  constructor(private storage: AngularFireStorage) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  protected uploadFile(event: any) {
+    const file: File = event.target.files[0];
+    // TODO Put the post ID into this filepath to have a separate folder per post.
+    // const filePath = `posts/${this.postID}`;
+    const filePath = `posts/${file.name}`;
+    const task = this.storage.upload(filePath, file, {
+      cacheControl: 'max-age=2592000,public',
+    });
+
+    task.snapshotChanges().subscribe();
   }
-
 }
--- src/app/whats-happening/whats-happening.component.ts
+++ src/app/whats-happening/whats-happening.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Component({
   selector: 'app-whats-happening',
@@ -9,22 +8,7 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
 })
 export class WhatsHappeningComponent implements OnInit {
   protected contentLoaded: boolean = true;
-  constructor(
-    private afs: AngularFirestore,
-    private storage: AngularFireStorage
-  ) {}
-
-  protected uploadFile(event: any) {
-    const file: File = event.target.files[0];
-    // TODO Put the post ID into this filepath to have a separate folder per post.
-    // const filePath = `posts/${this.postID}`;
-    const filePath = `posts/${file.name}`;
-    const task = this.storage.upload(filePath, file, {
-      cacheControl: 'max-age=2592000,public',
-    });
-
-    task.snapshotChanges().subscribe();
-  }
+  constructor(private afs: AngularFirestore) {}
 
   ngOnInit(): void {}
 }
