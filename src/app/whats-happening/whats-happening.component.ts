bb3e6230a63089ace6894c7b526cc91a2242c557
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-04-29 08:36:35 -0600

    Finish upload method

--- src/app/whats-happening/whats-happening.component.ts
+++ src/app/whats-happening/whats-happening.component.ts
@@ -16,6 +16,14 @@ export class WhatsHappeningComponent implements OnInit {
 
   protected uploadFile(event: any) {
     const file: File = event.target.files[0];
+    // TODO Put the post ID into this filepath to have a separate folder per post.
+    // const filePath = `posts/${this.postID}`;
+    const filePath = `posts/${file.name}`;
+    const task = this.storage.upload(filePath, file, {
+      cacheControl: 'max-age=2592000,public',
+    });
+
+    task.snapshotChanges().subscribe();
   }
 
   ngOnInit(): void {}
