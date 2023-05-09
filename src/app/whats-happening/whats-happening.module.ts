132a7576d1db968a4134777efdc7c9d9fd421980
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-05-09 12:21:40 -0600

    Add posts component

--- src/app/whats-happening/whats-happening.module.ts
+++ src/app/whats-happening/whats-happening.module.ts
@@ -8,9 +8,10 @@ import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AddPostComponent } from './add-post/add-post.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  declarations: [WhatsHappeningComponent, AddPostComponent],
+  declarations: [WhatsHappeningComponent, AddPostComponent, PostsComponent],
   imports: [
     CommonModule,
     WhatsHappeningRoutingModule,
