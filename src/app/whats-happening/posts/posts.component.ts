e4a0840fb08c8baf203d3f193d8671b1e8b313fa
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-05-12 11:41:45 -0600

    Add security logic

--- src/app/whats-happening/posts/posts.component.ts
+++ src/app/whats-happening/posts/posts.component.ts
@@ -2,6 +2,9 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Post } from '../post';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AddNewPostModalComponent } from '../add-new-post-modal/add-new-post-modal.component';
+import { Observable } from 'rxjs';
+import { AdministrationService } from 'src/app/services/administration.service';
+import { AuthService } from 'src/app/components/auth/auth.service';
 
 @Component({
   selector: 'app-posts',
@@ -11,10 +14,18 @@ import { AddNewPostModalComponent } from '../add-new-post-modal/add-new-post-mod
 export class PostsComponent implements OnInit {
   @Input() posts!: Post[] | null;
   @Output() addedNewPost: EventEmitter<any> = new EventEmitter();
+  protected currentUserIsAdmin$!: Observable<boolean>;
+  // protected userId: number;
 
-  constructor(private modalService: NgbModal) {}
+  constructor(
+    private modalService: NgbModal,
+    private adminService: AdministrationService,
+    protected authService: AuthService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.grabCurrentUserAdminStatus();
+  }
 
   protected addNewPost(): void {
     const modalRef = this.modalService.open(AddNewPostModalComponent);
@@ -24,5 +35,13 @@ export class PostsComponent implements OnInit {
         this.addedNewPost.emit();
       }
     });
+  }
+
+  private grabCurrentUserAdminStatus() {
+    if (this.authService.userId) {
+      this.currentUserIsAdmin$ = this.adminService.checkIfUserIsAdmin(
+        this.authService.userId
+      );
+    }
   }
 }
