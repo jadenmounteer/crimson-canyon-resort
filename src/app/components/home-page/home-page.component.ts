b1f7572bf349cbb36d5684b6ab148816f5c536bb
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-05-22 13:38:42 -0600

    Check if auth in home page

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent implements OnInit {
-  constructor(titleService: Title) {
+export class HomePageComponent implements OnInit, OnDestroy {
+  public isAuth: boolean = false;
+  private authSubscription!: Subscription;
+  constructor(titleService: Title, private authService: AuthService) {
     titleService.setTitle('Crimson Canyon Resort | Home');
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.setAuthSubscription();
+  }
+
+  private setAuthSubscription(): void {
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => {
+        this.isAuth = authStatus;
+      }
+    );
+    if (this.authService.isAuthenticated) {
+      this.isAuth = true;
+    } else {
+      this.isAuth = false;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
 }
