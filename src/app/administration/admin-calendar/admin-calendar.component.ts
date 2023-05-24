8afb92814fcef9a03af6f38078572ebae0a1e922
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-05-24 15:37:37 -0600

    Add component for calendar

--- src/app/administration/admin-calendar/admin-calendar.component.ts
+++ src/app/administration/admin-calendar/admin-calendar.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-admin-calendar',
   templateUrl: './admin-calendar.component.html',
-  styleUrls: ['./admin-calendar.component.scss']
+  styleUrls: ['./admin-calendar.component.scss'],
 })
 export class AdminCalendarComponent implements OnInit {
+  /**
+   * Here is the documentation for Angular Full Calendar: https://fullcalendar.io/docs/angular
+   * Here is an example project: https://github.com/fullcalendar/fullcalendar-examples
+   */
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',
+    weekends: true,
+    events: [{ title: 'Meeting', start: new Date() }],
+    handleWindowResize: true,
+    editable: true,
+    selectable: true,
+    /* you can update a remote database when these fire:
+  eventAdd:
+  eventChange:
+  eventRemove:
+  */
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/administration/administration-page/administration-page.component.ts
+++ src/app/administration/administration-page/administration-page.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AdministrationService } from '../../services/administration.service';
-import { CalendarOptions } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-administration-page',
@@ -10,25 +8,6 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 })
 export class AdministrationPageComponent implements OnInit {
   protected contentLoaded: boolean = false;
-
-  /**
-   * Here is the documentation for Angular Full Calendar: https://fullcalendar.io/docs/angular
-   * Here is an example project: https://github.com/fullcalendar/fullcalendar-examples
-   */
-  calendarOptions: CalendarOptions = {
-    plugins: [dayGridPlugin],
-    initialView: 'dayGridMonth',
-    weekends: true,
-    events: [{ title: 'Meeting', start: new Date() }],
-    handleWindowResize: true,
-    editable: true,
-    selectable: true,
-    /* you can update a remote database when these fire:
-    eventAdd:
-    eventChange:
-    eventRemove:
-    */
-  };
 
   constructor(private administrationService: AdministrationService) {}
 
