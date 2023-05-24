d90fa6022b6f4459b207b0df1525b808d0d95e65
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-05-24 15:47:19 -0600

    Copy code from example app

--- src/app/administration/admin-calendar/admin-calendar.component.ts
+++ src/app/administration/admin-calendar/admin-calendar.component.ts
@@ -1,6 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import {
+  CalendarOptions,
+  DateSelectArg,
+  EventApi,
+  EventClickArg,
+} from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import { INITIAL_EVENTS, createEventId } from './event-utils';
 
 @Component({
   selector: 'app-admin-calendar',
@@ -8,6 +14,7 @@ import dayGridPlugin from '@fullcalendar/daygrid';
   styleUrls: ['./admin-calendar.component.scss'],
 })
 export class AdminCalendarComponent implements OnInit {
+  calendarVisible = true;
   /**
    * Here is the documentation for Angular Full Calendar: https://fullcalendar.io/docs/angular
    * Here is an example project: https://github.com/fullcalendar/fullcalendar-examples
@@ -16,17 +23,62 @@ export class AdminCalendarComponent implements OnInit {
     plugins: [dayGridPlugin],
     initialView: 'dayGridMonth',
     weekends: true,
-    events: [{ title: 'Meeting', start: new Date() }],
+    events: INITIAL_EVENTS,
     handleWindowResize: true,
     editable: true,
     selectable: true,
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: this.handleEvents.bind(this),
     /* you can update a remote database when these fire:
   eventAdd:
   eventChange:
   eventRemove:
   */
   };
-  constructor() {}
+  currentEvents: EventApi[] = [];
+  constructor(private changeDetector: ChangeDetectorRef) {}
 
   ngOnInit(): void {}
+
+  handleCalendarToggle() {
+    this.calendarVisible = !this.calendarVisible;
+  }
+
+  handleWeekendsToggle() {
+    const { calendarOptions } = this;
+    calendarOptions.weekends = !calendarOptions.weekends;
+  }
+
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (
+      confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  }
+
+  handleEvents(events: EventApi[]) {
+    this.currentEvents = events;
+    this.changeDetector.detectChanges();
+  }
 }
