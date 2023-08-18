777499d081a08eda4e1d18e1708c351aba857f81
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-08-18 16:45:43 -0600

    Get lazy loading working

--- src/app/whats-happening-page/whats-happening-page.module.ts
+++ src/app/whats-happening-page/whats-happening-page.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WhatsHappeningPageComponent } from './whats-happening-page/whats-happening-page.component';
-
-
+import { WhatsHappeningPageRoutingModule } from './whats-happening-page-routing.module';
 
 @NgModule({
-  declarations: [
-    WhatsHappeningPageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [WhatsHappeningPageComponent],
+  imports: [CommonModule, WhatsHappeningPageRoutingModule],
 })
-export class WhatsHappeningPageModule { }
+export class WhatsHappeningPageModule {}
