14902abf9847a4c6927d2d91eac6b4bd93a67e86
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-08-18 17:03:01 -0600

    Add whats happening page

--- src/app/whats-happening-page/whats-happening-page.module.ts
+++ src/app/whats-happening-page/whats-happening-page.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WhatsHappeningPageComponent } from './whats-happening-page/whats-happening-page.component';
 import { WhatsHappeningPageRoutingModule } from './whats-happening-page-routing.module';
+import { InstagramFeedModule } from '../components/instagram-feed/instagram-feed.module';
+import { UiComponentsModule } from '../ui-components/ui-components.module';
 
 @NgModule({
   declarations: [WhatsHappeningPageComponent],
-  imports: [CommonModule, WhatsHappeningPageRoutingModule],
+  imports: [
+    CommonModule,
+    WhatsHappeningPageRoutingModule,
+    InstagramFeedModule,
+    UiComponentsModule,
+  ],
 })
 export class WhatsHappeningPageModule {}
--- src/app/whats-happening-page/whats-happening-page/whats-happening-page.component.ts
+++ src/app/whats-happening-page/whats-happening-page/whats-happening-page.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { InstagramFeedService } from 'src/app/components/instagram-feed/instagram-feed.service';
 
 @Component({
   selector: 'app-whats-happening-page',
   templateUrl: './whats-happening-page.component.html',
-  styleUrls: ['./whats-happening-page.component.scss']
+  styleUrls: ['./whats-happening-page.component.scss'],
 })
-export class WhatsHappeningPageComponent implements OnInit {
+export class WhatsHappeningPageComponent implements OnInit, OnDestroy {
+  protected loading: boolean = true;
+  private instaSubscription!: Subscription;
+  protected instaData: Array<any> | undefined;
 
-  constructor() { }
+  constructor(private instagramFeedService: InstagramFeedService) {}
 
   ngOnInit(): void {
+    this.instaSubscription = this.instagramFeedService
+      .getHomePageInstaData()
+      .subscribe((data) => {
+        console.log(data);
+        this.instaData = data.data;
+        console.log(this.instaData);
+        this.loading = false;
+      });
   }
 
+  ngOnDestroy(): void {
+    this.instaSubscription.unsubscribe();
+  }
 }
