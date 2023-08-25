b325cc3f3d3b36e66c7f2a4865d0534adabc6ef1
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-08-25 09:39:25 -0600

    Add deploy to firebase script

--- src/scripts/deploy-to-github-pages.sh
+++ src/scripts/deploy-to-github-pages.sh
@@ -1,4 +1,5 @@
-cd /Users/jadenmounteer/git/crimson-canyon-resort
-npm i angular-cli-ghpages --save-prod
-ng build --base-href "https://jadenmounteer.github.io/crimson-canyon-resort/"
-npx angular-cli-ghpages --dir=dist/crimson-canyon-resort
+# This script is deprecated.
+#cd /Users/jadenmounteer/git/crimson-canyon-resort
+#npm i angular-cli-ghpages --save-prod
+#ng build --base-href "https://jadenmounteer.github.io/crimson-canyon-resort/"
+#npx angular-cli-ghpages --dir=dist/crimson-canyon-resort
