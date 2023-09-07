32006cf8f0c1ce5f995e9eacbd8331ffe25e6c9d
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-09-07 12:17:44 -0600

    More fitness center updates

--- src/app/fitness-center/fitness-center-page/fitness-center-page.component.ts
+++ src/app/fitness-center/fitness-center-page/fitness-center-page.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-fitness-center-page',
   templateUrl: './fitness-center-page.component.html',
-  styleUrls: ['./fitness-center-page.component.scss']
+  styleUrls: ['./fitness-center-page.component.scss'],
 })
 export class FitnessCenterPageComponent implements OnInit {
+  protected loading: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
