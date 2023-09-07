72349dc21798c5d5d97b3a8056c429b15025b184
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-09-07 13:04:59 -0600

    More leader board updates

--- src/app/fitness-center/add-or-edit-leader-board-modal/add-or-edit-leader-board-modal.component.ts
+++ src/app/fitness-center/add-or-edit-leader-board-modal/add-or-edit-leader-board-modal.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { LeaderBoard } from 'src/app/types/leaderboard';
 
 @Component({
   selector: 'app-add-or-edit-leader-board-modal',
   templateUrl: './add-or-edit-leader-board-modal.component.html',
-  styleUrls: ['./add-or-edit-leader-board-modal.component.scss']
+  styleUrls: ['./add-or-edit-leader-board-modal.component.scss'],
 })
 export class AddOrEditLeaderBoardModalComponent implements OnInit {
+  @Input() title: string = 'Add New Leader Board 🏋️‍♀️';
+  @Input() leaderBoardToEdit: LeaderBoard | undefined;
+  constructor(public activeModal: NgbActiveModal) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/fitness-center/leaderboard-section/leaderboard-section.component.ts
+++ src/app/fitness-center/leaderboard-section/leaderboard-section.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AddOrEditLeaderBoardModalComponent } from '../add-or-edit-leader-board-modal/add-or-edit-leader-board-modal.component';
+import { LeaderBoard } from 'src/app/types/leaderboard';
 
 @Component({
   selector: 'app-leaderboard-section',
@@ -11,6 +12,18 @@ export class LeaderboardSectionComponent implements OnInit {
   constructor(private modalService: NgbModal) {}
 
   ngOnInit(): void {}
+
+  protected editLeaderBoard(leaderBoard: LeaderBoard): void {
+    const modalRef = this.modalService.open(AddOrEditLeaderBoardModalComponent);
+    modalRef.componentInstance.leaderBoardToEdit = leaderBoard;
+    modalRef.componentInstance.title = `Edit ${leaderBoard.name}}`;
+
+    modalRef.result.then((result) => {
+      if (result === 'success') {
+        // this.addedNewPost.emit();
+      }
+    });
+  }
 
   protected createNewLeaderBoard(): void {
     const modalRef = this.modalService.open(AddOrEditLeaderBoardModalComponent);
