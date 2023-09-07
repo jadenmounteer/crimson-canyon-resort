6c3b8e23468605ed4cc3be0c3a2aea50bc979545
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-09-07 13:42:16 -0600

    Make leaderboards load on create

--- src/app/fitness-center/leaderboard-section/leaderboard-section.component.ts
+++ src/app/fitness-center/leaderboard-section/leaderboard-section.component.ts
@@ -44,7 +44,7 @@ export class LeaderboardSectionComponent implements OnInit {
 
     modalRef.result.then((result) => {
       if (result === 'success') {
-        // this.addedNewPost.emit();
+        this.loadLeaderBoards();
       }
     });
   }
