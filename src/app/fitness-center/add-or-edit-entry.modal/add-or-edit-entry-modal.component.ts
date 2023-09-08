4b9624abbed9eb30ce8f06f332c14bd3600c1c26
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-09-08 17:01:42 -0600

    Add more updates to entry modal

--- src/app/fitness-center/add-or-edit-entry.modal/add-or-edit-entry-modal.component.ts
+++ src/app/fitness-center/add-or-edit-entry.modal/add-or-edit-entry-modal.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from 'src/app/components/auth/auth.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { LeaderBoard, LeaderBoardEntry } from 'src/app/types/leaderboard';
 
 @Component({
   selector: 'app-add-or-edit-entry-modal',
@@ -8,11 +11,30 @@ import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AddOrEditEntryModalComponent implements OnInit {
   @Input() title: string = 'Add New Leader Board Entry 🏋️‍♀️';
+  @Input() leaderBoard!: LeaderBoard;
+
+  protected newEntry: Partial<LeaderBoardEntry> = {
+    userId: '',
+    individualName: '',
+    date: new Date(),
+    score: 0,
+  };
+
   protected loading: boolean = true;
   constructor(
     public activeModal: NgbActiveModal,
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private authService: AuthService,
+    private angularFirestore: AngularFirestore
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.checkIfEntryExists();
+    this.loading = false;
+  }
+
+  private checkIfEntryExists() {
+    // TODO check if an entry exists for this user on this leaderboard
+    // TODO set the entry data and title accordingly
+  }
 }
