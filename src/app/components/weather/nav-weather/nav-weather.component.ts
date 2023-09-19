f5f0c79ec7fce5d5e14af896274b0e5890f063f8
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-09-19 16:45:02 -0600

    Get current temp

--- src/app/components/weather/nav-weather/nav-weather.component.ts
+++ src/app/components/weather/nav-weather/nav-weather.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IconService } from 'src/app/services/icon.service';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-nav-weather',
@@ -7,9 +8,19 @@ import { IconService } from 'src/app/services/icon.service';
   styleUrls: ['./nav-weather.component.scss'],
 })
 export class NavWeatherComponent implements OnInit {
+  public currentWeatherData: any;
   weatherIcon: string = 'faSun';
+  currentTemperature: string = '0';
 
-  constructor(public icon: IconService) {}
+  constructor(
+    public icon: IconService,
+    private weatherService: WeatherService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.weatherService.getCurrentWeather().subscribe((data) => {
+      this.currentWeatherData = data;
+      this.currentTemperature = this.currentWeatherData.main.temp;
+    });
+  }
 }
