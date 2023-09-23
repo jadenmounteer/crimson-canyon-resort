b9ceff19416546db730cbbcc82d2c4201d21cb38
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-09-23 09:53:50 -0600

    Add countdown

--- src/app/administration/countdown-to-instagram-token-refresh/countdown-to-instagram-token-refresh.component.ts
+++ src/app/administration/countdown-to-instagram-token-refresh/countdown-to-instagram-token-refresh.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-countdown-to-instagram-token-refresh',
   templateUrl: './countdown-to-instagram-token-refresh.component.html',
-  styleUrls: ['./countdown-to-instagram-token-refresh.component.scss']
+  styleUrls: ['./countdown-to-instagram-token-refresh.component.scss'],
 })
-export class CountdownToInstagramTokenRefreshComponent implements OnInit {
+export class CountdownToInstagramTokenRefreshComponent {
+  protected dateOfLastRefresh = new Date('2023-09-23T00:00:00');
+  private todaysDate = new Date();
+  protected daysUntilTokenExpires: number =
+    this.getDaysUntilTokenExpiresFromDateOfLastRefresh(5184000);
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  private getDaysUntilTokenExpiresFromDateOfLastRefresh(
+    secondsUntilTokenExpires: number
+  ): number {
+    const secondsInADay = 86400;
+    const daysUntilTokenExpires =
+      (secondsUntilTokenExpires -
+        (this.todaysDate.getTime() - this.dateOfLastRefresh.getTime()) / 1000) /
+      secondsInADay;
+    return Math.round(daysUntilTokenExpires);
   }
-
 }
