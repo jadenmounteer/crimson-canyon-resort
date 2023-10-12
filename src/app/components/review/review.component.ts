d0cdce84781f8e1e4545a166311831a339b56250
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-10-11 21:29:19 -0600

    More updates for reviews

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Review } from './review.type';
 
 @Component({
   selector: 'app-review',
   templateUrl: './review.component.html',
-  styleUrls: ['./review.component.scss']
+  styleUrls: ['./review.component.scss'],
 })
 export class ReviewComponent implements OnInit {
+  @Input() review!: Review;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/components/reviews-section/reviews-section.component.ts
+++ src/app/components/reviews-section/reviews-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Review } from '../review/review.type';
 
 @Component({
   selector: 'app-reviews-section',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsSectionComponent implements OnInit {
   protected loading: boolean = false;
+  protected reviews: Review[] = [];
 
   constructor() {}
 
