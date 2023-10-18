45277a4d7941c4432372de0b87ed11103725de42
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-10-17 21:17:13 -0600

    Add initial animation

--- src/app/components/reviews-section/reviews-section.component.ts
+++ src/app/components/reviews-section/reviews-section.component.ts
@@ -5,16 +5,35 @@ import { AddReviewModalComponent } from '../add-review-modal/add-review-modal.co
 import { AuthService } from '../auth/auth.service';
 import { Observable } from 'rxjs';
 import { ReviewService } from 'src/app/services/review.service';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-reviews-section',
   templateUrl: './reviews-section.component.html',
   styleUrls: ['./reviews-section.component.scss'],
+  animations: [
+    trigger('slideInRight', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('1000ms ease-out', style({ transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', [
+        animate('1000ms ease-out', style({ transform: 'translateX(-100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class ReviewsSectionComponent implements OnInit {
   protected loading: boolean = true;
   protected reviews$!: Observable<Review[]>;
   @Input() isAuth: boolean = false;
+  protected imageVisible = true;
 
   constructor(
     private modalService: NgbModal,
@@ -24,6 +43,10 @@ export class ReviewsSectionComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadReviews();
+  }
+
+  protected toggleImage() {
+    this.imageVisible = !this.imageVisible;
   }
 
   private loadReviews(): void {
