21909106a90b9751faed6ab325b63c9c8f8d2f23
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-12-19 22:59:49 -0700

    Functions!

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,57 @@
 const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+require("dotenv").config();
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const { SENDER_EMAIL, SENDER_PASSWORD } = process.env;
+
+exports.sendEmailNotification = functions.firestore
+  .document("posts/{docId}")
+  .onCreate((snap, context) => {
+    const notification = snap.data();
+
+    let authData = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: SENDER_EMAIL,
+        pass: SENDER_PASSWORD,
+      },
+    });
+
+    authData.sendMail({
+      from: SENDER_EMAIL,
+      to: "mounteerjaden@gmail.com", // notification is the firebase document. So I can have an array of emails to send to in the announcement document and loop through them and call send mail on each one. For example: notification.emails.forEach(email => sendMail(email))
+      subject: "Hello from Crimson Canyon Resort", // notification.subject
+      html: `<h1>${notification.createdByUserName} just posted an announcement</h1></br><p>${notification.message}</p></br>-<a href="https://crimson-canyon-resort-prod.web.app/">Crimson Canyon Resort</a>`,
+    });
+  });
+
+// TODO send email when email is approved to create account
+exports.sendEmailNotification = functions.firestore
+  .document("accessRequests/{docId}")
+  .onUpdate((snap, context) => {
+    // If notification.approved = true, send email to notification.email
+    const notification = snap.data();
+
+    if (!notification.approved) {
+      let authData = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: SENDER_EMAIL,
+          pass: SENDER_PASSWORD,
+        },
+      });
+
+      authData.sendMail({
+        from: SENDER_EMAIL,
+        to: `${notification.email}`, // notification is the firebase document. So I can have an array of emails to send to in the announcement document and loop through them and call send mail on each one. For example: notification.emails.forEach(email => sendMail(email))
+        subject: "Your email has been approved!", // notification.subject
+        html: `<p>Your email, ${notification.email}, has just been approved to create an account at Crimson Canyon Resort.</br><a href="https://crimson-canyon-resort-prod.web.app/">Create an account here</a>`,
+      });
+    }
+  });
