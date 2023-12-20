d666d776c77b249404ac4154d2769d2df44412f8
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-12-20 10:29:37 -0700

    change

--- functions/index.js
+++ functions/index.js
@@ -25,33 +25,33 @@ exports.sendEmailNotification = functions.firestore
       from: SENDER_EMAIL,
       to: "mounteerjaden@gmail.com", // notification is the firebase document. So I can have an array of emails to send to in the announcement document and loop through them and call send mail on each one. For example: notification.emails.forEach(email => sendMail(email))
       subject: "Hello from Crimson Canyon Resort", // notification.subject
-      html: `<h1>${notification.createdByUserName} just posted an announcement</h1></br><p>${notification.message}</p></br>-<a href="https://crimson-canyon-resort-prod.web.app/">Crimson Canyon Resort</a>`,
+      html: `<h1>${notification.createdByUserName} just posted an announcement</h1></br><p>${notification.message}</p></br>- <a href="https://crimson-canyon-resort-prod.web.app/">Crimson Canyon Resort</a>`,
     });
   });
 
 // TODO send email when email is approved to create account
-exports.sendEmailNotification = functions.firestore
-  .document("accessRequests/{docId}")
-  .onUpdate((snap, context) => {
-    // If notification.approved = true, send email to notification.email
-    const notification = snap.data();
+// exports.sendEmailApprovalNotification = functions.firestore
+//   .document("accessRequests/{docId}")
+//   .onUpdate((snap, context) => {
+//     // If notification.approved = true, send email to notification.email
+//     const notification = snap.data();
 
-    if (!notification.approved) {
-      let authData = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-          user: SENDER_EMAIL,
-          pass: SENDER_PASSWORD,
-        },
-      });
+//     if (!notification.approved) {
+//       let authData = nodemailer.createTransport({
+//         host: "smtp.gmail.com",
+//         port: 465,
+//         secure: true,
+//         auth: {
+//           user: SENDER_EMAIL,
+//           pass: SENDER_PASSWORD,
+//         },
+//       });
 
-      authData.sendMail({
-        from: SENDER_EMAIL,
-        to: `${notification.email}`, // notification is the firebase document. So I can have an array of emails to send to in the announcement document and loop through them and call send mail on each one. For example: notification.emails.forEach(email => sendMail(email))
-        subject: "Your email has been approved!", // notification.subject
-        html: `<p>Your email, ${notification.email}, has just been approved to create an account at Crimson Canyon Resort.</br><a href="https://crimson-canyon-resort-prod.web.app/">Create an account here</a>`,
-      });
-    }
-  });
+//       authData.sendMail({
+//         from: SENDER_EMAIL,
+//         to: `${notification.email}`, // notification is the firebase document. So I can have an array of emails to send to in the announcement document and loop through them and call send mail on each one. For example: notification.emails.forEach(email => sendMail(email))
+//         subject: "Your email has been approved!", // notification.subject
+//         html: `<p>Your email, ${notification.email}, has just been approved to create an account at Crimson Canyon Resort.</br><a href="https://crimson-canyon-resort-prod.web.app/">Create an account here</a>`,
+//       });
+//     }
+//   });
