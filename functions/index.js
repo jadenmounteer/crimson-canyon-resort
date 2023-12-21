4e90492fe98512d7455f01c3aaecdb0b0326998f
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-12-20 20:56:22 -0700

    Add error log to function

--- functions/index.js
+++ functions/index.js
@@ -21,12 +21,19 @@ exports.sendEmailNotification = functions.firestore
       },
     });
 
-    authData.sendMail({
-      from: SENDER_EMAIL,
-      to: "mounteerjaden@gmail.com", // notification is the firebase document. So I can have an array of emails to send to in the announcement document and loop through them and call send mail on each one. For example: notification.emails.forEach(email => sendMail(email))
-      subject: "Hello from Crimson Canyon Resort", // notification.subject
-      html: `<h1>${notification.createdByUserName} just posted an announcement</h1></br><p>${notification.message}</p></br>- <a href="https://crimson-canyon-resort-prod.web.app/">Crimson Canyon Resort</a>`,
-    });
+    authData
+      .sendMail({
+        from: SENDER_EMAIL,
+        to: "mounteerjaden@gmail.com", // notification is the firebase document. So I can have an array of emails to send to in the announcement document and loop through them and call send mail on each one. For example: notification.emails.forEach(email => sendMail(email))
+        subject: "Hello from Crimson Canyon Resort", // notification.subject
+        html: `<h1>${notification.createdByUserName} just posted an announcement</h1></br><p>${notification.message}</p></br>- <a href="https://crimson-canyon-resort-prod.web.app/">Crimson Canyon Resort</a>`,
+      })
+      .then((res) => {
+        console.log("Successfully sent email.");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 
 // TODO send email when email is approved to create account
