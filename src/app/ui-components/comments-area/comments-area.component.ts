a6da7ced8c92d824aa7a5215550a3e0711133150
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-12-23 07:37:33 -0700

    Finalize comments

--- src/app/ui-components/comments-area/comments-area.component.ts
+++ src/app/ui-components/comments-area/comments-area.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { PostComment } from '../../announcements/post';
+import { Post, PostComment } from '../../announcements/post';
 import { IconService } from 'src/app/services/icon.service';
-import { throwError } from 'rxjs';
+import { AnnouncementsService } from 'src/app/announcements/announcements.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AuthService } from 'src/app/components/auth/auth.service';
 
 @Component({
   selector: 'app-comments-area',
@@ -9,13 +11,42 @@ import { throwError } from 'rxjs';
   styleUrls: ['./comments-area.component.scss'],
 })
 export class CommentsAreaComponent implements OnInit {
-  @Input() comments: PostComment[] | undefined = [];
+  @Input() post!: Post;
 
   protected newComment: Partial<PostComment> = {
     message: '',
   };
 
-  constructor(public icon: IconService) {}
+  constructor(
+    public icon: IconService,
+    private announcementsService: AnnouncementsService,
+    private angularFirestore: AngularFirestore,
+    private authService: AuthService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (!this.post) {
+      throw new Error('Post is required');
+    }
+  }
+
+  addComment() {
+    this.newComment.id = this.angularFirestore.createId();
+    this.newComment.createdDate = Date.now();
+    this.newComment.createdByUserName = this.authService.userDisplayName;
+
+    if (!this.post.comments) {
+      this.post.comments = [];
+    }
+
+    this.post.comments.push(this.newComment as PostComment);
+
+    this.announcementsService
+      .updatePost(this.post.id, this.post)
+      .subscribe((res) => {
+        this.newComment = {
+          message: '',
+        };
+      });
+  }
 }
