7aec33909e4a6408655402ab2f86c3231ed8aa67
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2023-12-23 08:28:18 -0700

    Add funny comment suggestions

--- src/app/ui-components/comments-area/comments-area.component.ts
+++ src/app/ui-components/comments-area/comments-area.component.ts
@@ -17,12 +17,17 @@ export class CommentsAreaComponent implements OnInit {
     message: '',
   };
 
+  protected randomFunnyPlaceholderMessage = '';
+
   constructor(
     public icon: IconService,
     private announcementsService: AnnouncementsService,
     private angularFirestore: AngularFirestore,
     private authService: AuthService
-  ) {}
+  ) {
+    this.randomFunnyPlaceholderMessage =
+      this.generateRandomFunnyPlaceholderMessage();
+  }
 
   ngOnInit(): void {
     if (!this.post) {
@@ -30,7 +35,7 @@ export class CommentsAreaComponent implements OnInit {
     }
   }
 
-  addComment() {
+  protected addComment() {
     this.newComment.id = this.angularFirestore.createId();
     this.newComment.createdDate = Date.now();
     this.newComment.createdByUserName = this.authService.userDisplayName;
@@ -47,6 +52,37 @@ export class CommentsAreaComponent implements OnInit {
         this.newComment = {
           message: '',
         };
+        this.randomFunnyPlaceholderMessage =
+          this.generateRandomFunnyPlaceholderMessage();
       });
+  }
+
+  private generateRandomFunnyPlaceholderMessage(): string {
+    const messages = [
+      "Please type a comment for everyone to see. Please keep it workplace appropriate...just kidding! We're all family here! 🤪",
+      'What are you waiting for? Type a comment already! ⏰',
+      'Ok, a nerd spent a ton of time building a comment section. Please use it by adding a comment! 🤓',
+      "Add a comment. Don't be shy! 🤗",
+      "Add a comment. We promise we won't judge you! 🤞",
+      'Add a comment. We know you have something to say! 🗣',
+      'Release your inner thoughts and add a comment! 💭',
+      'Comments are magical. Add one! 🧙‍♂️',
+      'Add a comment. We know you want to! 🤫',
+      'Here\'s a suggestion for a comment: "I love this app!" 🥰',
+      'Here\'s a suggestion for a comment: "This app is awesome!" 🤩',
+      'Here\'s a suggestion for a comment: "This app is the bomb dot com!" 💣',
+      'Here\'s a suggestion for a comment: "Confess your inner love for pizza!" 🍕',
+      'Here\'s a suggestion for a comment: "Write a haiku about tacos!" 🌮',
+      'Here\'s a suggestion for a comment: "Tell everyone about the craziest dream you\'ve ever had." 💤',
+      'Here\'s a suggestion for a comment: "Tell everyone your opinion on the last movie you watched." 🎬',
+      'Here\'s a suggestion for a comment: "Tell everyone your opinion on the the last book you read." 📚',
+      'Life advice: write a comment. We can all be like your therapist or something. 🤷‍♂️',
+      'Life advice: writing comments frees your inner soul. Share your soul with us. We want it! 🧘‍♀️',
+      "Write a comment. I'll be your best friend. 🤝",
+      'Free hugs for those who write comments! 🤗',
+    ];
+
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    return messages[randomIndex];
   }
 }
