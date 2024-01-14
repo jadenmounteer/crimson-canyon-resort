a27a798b7d34c00e72524887f59d94b9b6cae8d0
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-01-13 21:51:56 -0700

    Add admin info link button

--- src/app/components/admin-section/admin-section.component.ts
+++ src/app/components/admin-section/admin-section.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-section',
   templateUrl: './admin-section.component.html',
-  styleUrls: ['./admin-section.component.scss']
+  styleUrls: ['./admin-section.component.scss'],
 })
 export class AdminSectionComponent implements OnInit {
+  protected contentLoaded: boolean = false;
 
-  constructor() { }
+  constructor(protected router: Router) {}
 
   ngOnInit(): void {
+    this.contentLoaded = true;
   }
-
 }
